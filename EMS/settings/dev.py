c24c4f5dbe86e0acdad7658870cb673e3270cfa5
Abhishek Thrormothe <117808499+abhi2k4@users.noreply.github.com>
2024-01-01 15:53:13 +0530

    Update dev.py

--- EMS/settings/dev.py
+++ EMS/settings/dev.py
@@ -3,7 +3,7 @@ from .common import *
 
 SECRET_KEY = 'django-insecure-zz#59)91aa$bp$=7sd7udm!!1!f2q-zu6oftf&$@ln8tox-49p'
 
+
 DEBUG= True
 
-ALLOWED_HOSTS = ['localhost', '8000-mejam35-emsojus-4cola7f4eku.ws-us107.gitpod.io']
-
+ALLOWED_HOSTS = ['localhost' ,'ojussports.eventsapsit.org']
