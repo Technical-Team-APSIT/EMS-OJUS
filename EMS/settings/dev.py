ac6791bda6d2d46dea27fab3a803cc855e8ea362
Sahil Sonar <sss.sonar2003@gmail.com>
2024-01-01 06:42:36 +0000

    Stuff

--- EMS/settings/dev.py
+++ EMS/settings/dev.py
@@ -6,5 +6,5 @@ SECRET_KEY = 'django-insecure-zz#59)91aa$bp$=7sd7udm!!1!f2q-zu6oftf&$@ln8tox-49p
 
 DEBUG= True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '8000-mejam35-emsojus-4cola7f4eku.ws-us107.gitpod.io']
 
