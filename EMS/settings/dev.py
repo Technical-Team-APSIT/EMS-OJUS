f2096ab3b9c670d876b5a9ba9f3fda12571d4238
Abhishek Thrormothe <117808499+abhi2k4@users.noreply.github.com>
2024-02-11 00:09:43 +0530

    font file

--- EMS/settings/dev.py
+++ EMS/settings/dev.py
@@ -6,4 +6,4 @@ SECRET_KEY = 'django-insecure-zz#59)91aa$bp$=7sd7udm!!1!f2q-zu6oftf&$@ln8tox-49p
 
 DEBUG= True
 
-ALLOWED_HOSTS = ['localhost' ,'ojussports.eventsapsit.org']
+ALLOWED_HOSTS = ['127.0.0.1' ,'ojussports.eventsapsit.org']
